30b64077a988e6130c2f591e9029b3fbfd374036
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-11-29 01:14:57 +0530

    fix: create css positions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {GridSystem} from "./pages/GridSystem";
+import {Positions} from "./pages/Positions";
 
 function App() {
     return (
         <div className="App">
-            <div className={"main-title"}>Bootstrap - Grid System</div>
-            <GridSystem/>
+            <div className={"main-title"}>CSS Positions</div>
+            <Positions/>
             <img src="/assets/images/logo.png" alt="logo" className={"bg-logo"}/>
             <div className={"program-name left"}>Coder Catalyst Program</div>
             <div className={"program-name right"}>UI Practices</div>
