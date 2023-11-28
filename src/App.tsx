f158521b3b008fa2922ae7e898e735a78d8820f6
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-11-28 17:08:19 +0530

    fix: wrong component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Breakpoints} from "./pages/Breakpoints";
+import {GridSystem} from "./pages/GridSystem";
 
 function App() {
     return (
         <div className="App">
             <div className={"main-title"}>Bootstrap - Grid System Breakpoints</div>
-            <Breakpoints/>
+            <GridSystem/>
             <img src="/assets/images/logo.png" alt="logo" className={"bg-logo"}/>
             <div className={"program-name left"}>Coder Catalyst Program</div>
             <div className={"program-name right"}>UI Practices</div>
