12be39a97b90519400d8dda72cfc55e8818f9ccd
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-11-28 11:13:41 +0530

    fix: issues in grid system page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import GridSystem from "./pages/GridSystem";
+import {GridSystem} from "./pages/GridSystem";
 
 function App() {
     return (
--- src/pages/GridSystem.tsx
+++ src/pages/GridSystem.tsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-function GridSystem() {
+export function GridSystem() {
     return (
         <div className="main-container">
             <Container>
@@ -31,7 +31,5 @@ function GridSystem() {
                 </Row>
             </Container>
         </div>
-    );
+    )
 }
-
-export default GridSystem;
